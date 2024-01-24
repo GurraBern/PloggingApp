eb1cdab665dc6d4adcffa0d5f8b02f06a1fb114f
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2024-01-24 11:12:57 +0100

    [Add] - Setup project

---
+++ PloggingApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PloggingApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PloggingApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PloggingApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
