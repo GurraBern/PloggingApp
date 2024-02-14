0edfd9ab33511b378433e4cc494c44a64652595b
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2024-02-14 21:04:19 +0100

    [GUI] - Improved styling for  displaying the rankings

--- PloggingApp/App.xaml.cs
+++ PloggingApp/App.xaml.cs
@@ -7,6 +7,9 @@
             InitializeComponent();
 
             MainPage = new AppShell();
+
+            //TODO remove if we want to allow both Light mode and Dark mode
+            UserAppTheme = AppTheme.Light;
         }
     }
 }
---
+++ PloggingApp/Converters/FirstTwoCharsConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace PloggingApp.Converters;
+
+public class FirstTwoCharsConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var str = value as string;
+        return str?.Substring(0, Math.Min(2, str.Length)) ?? "";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
