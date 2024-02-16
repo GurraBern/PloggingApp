a0bc1e7439027d84fa4edbbf9a3af54d9894506a
Gustav Jungstedt <gustav.jungstedt@gmail.com>
2024-02-16 10:57:03 +0100

    försöker displaya en map på map page

--- PloggingApp/MVVM/ViewModels/MapViewModel.cs
+++ PloggingApp/MVVM/ViewModels/MapViewModel.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace PloggingApp.MVVM.ViewModels
 {
-    internal class MapViewModel
+    public class MapViewModel
     {
+
     }
 }
