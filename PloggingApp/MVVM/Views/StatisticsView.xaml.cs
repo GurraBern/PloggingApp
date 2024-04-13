c1137f910822921a564e6a2019dc0d174badec85
AlbinHal <albin.hallin1@gmail.com>
2024-04-13 13:09:09 +0200

    [FEAT] Added radiobuttons to statisticspage.

--- PloggingApp/MVVM/Views/StatisticsView.xaml.cs
+++ PloggingApp/MVVM/Views/StatisticsView.xaml.cs
@@ -1,12 +1,34 @@
 using Microcharts;
+using PloggingApp.MVVM.ViewModels;
 using SkiaSharp;
 
 namespace PloggingApp.MVVM.Views;
 
 public partial class StatisticsView : ContentView
 {
+	private StatisticsViewModel viewModel {  get; set; }
 	public StatisticsView()
 	{
 		InitializeComponent();
 	}
-}
+
+	private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+	{
+		if (viewModel is null)
+		{
+			if (BindingContext != null)
+				viewModel = (StatisticsViewModel)BindingContext;
+			else
+				return;
+		}
+		RadioButton radioButton = sender as RadioButton;
+		if (radioButton.ContentAsString() == "Year")
+		{
+			viewModel.ShowYearCommand.Execute(null);
+		}
+		else
+		{
+			viewModel.ShowMonthCommand.Execute(null);
+		}
+	}
+} 
