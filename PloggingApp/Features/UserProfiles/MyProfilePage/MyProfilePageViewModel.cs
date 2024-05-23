fc81977963908ed6fccce990a8a48a10ced61f00
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2024-05-23 21:37:27 +0200

    [Ref] - Refactored MyProfilePage for badges and history

--- PloggingApp/Features/UserProfiles/MyProfilePage/MyProfilePageViewModel.cs
+++ PloggingApp/Features/UserProfiles/MyProfilePage/MyProfilePageViewModel.cs
@@ -1,11 +1,17 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Plogging.Core.Models;
+using PloggingApp.Data.Services.Interfaces;
+using PloggingApp.Extensions;
+using PloggingApp.Features.Statistics;
 using PloggingApp.Features.UserProfiles.Badges;
+using PloggingApp.Services.Authentication;
 using PloggingApp.Shared;
+using System.Collections.ObjectModel;
 
 namespace PloggingApp.Features.UserProfiles;
 
-public partial class MyProfilePageViewModel: BaseViewModel
+public partial class MyProfilePageViewModel : BaseViewModel, IAsyncInitialization
 {
     [ObservableProperty]
     private ProfileInfoViewModel profileInfoViewModel;
@@ -13,18 +19,37 @@ public partial class MyProfilePageViewModel: BaseViewModel
     [ObservableProperty]
     private BadgesViewModel badgesViewModel;
 
-    public MyProfilePageViewModel(ProfileInfoViewModel profileInfoViewModel, BadgesViewModel badgesViewModel)
+    public ObservableCollection<PlogSession> LatestSessions { get; set; } = [];
+
+    private readonly IPloggingSessionService _ploggingSessionService;
+    private readonly IAuthenticationService _authenticationService;
+
+    [ObservableProperty]
+    private PloggingStatistics ploggingStatistics;
+
+    public Task Initialization {  get; private set; }
+
+    public MyProfilePageViewModel(ProfileInfoViewModel profileInfoViewModel, BadgesViewModel badgesViewModel, IPloggingSessionService ploggingSessionService, IAuthenticationService authenticationService)
     {
         ProfileInfoViewModel = profileInfoViewModel;
         BadgesViewModel = badgesViewModel;
+        _ploggingSessionService = ploggingSessionService;
+        _authenticationService = authenticationService;
+
+        Initialization = Initialize();
     }
 
-    [RelayCommand]
-    private async Task Refresh()
+    private async Task Initialize()
     {
-        IsBusy = true;
-        //IsRefreshing = false;
-        IsBusy = false;
+        var plogSessions = await _ploggingSessionService.GetUserSessions(_authenticationService.UserId, DateTime.UtcNow.AddYears(-1), DateTime.UtcNow);
+
+        SetupStatistics(plogSessions);
+        LatestSessions.ClearAndAddRange(plogSessions.Take(3));
+    }
+
+    private void SetupStatistics(IEnumerable<PlogSession> plogSessions)
+    {
+        PloggingStatistics = new PloggingStatistics(plogSessions.Where(s => s.StartDate.Month == DateTime.Now.Month));
     }
 
     [RelayCommand]
@@ -32,32 +57,4 @@ public partial class MyProfilePageViewModel: BaseViewModel
     {
         await Shell.Current.GoToAsync($"{nameof(HistoryPage)}");
     }
-
-   
-    //[RelayCommand]
-    //private async Task Logout()
-    //{
-    //    bool response = await Application.Current.MainPage.DisplayAlert("Signing out", "Are you sure you want to logout?", "Yes", "No");
-
-    //    if (response)
-    //    {
-    //        _authenticationService.SignOut();
-    //        await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
-    //    }
-    //}
-
-    //[RelayCommand]
-    //private async Task Refresh()
-    //{
-    //    IsBusy = true;
-    //    await GetSessions();
-    //    IsRefreshing = false;
-    //    IsBusy = false;
-    //}
-
-    //[RelayCommand]
-    //private async Task GoToHistoryPage()
-    //{
-    //    await Shell.Current.GoToAsync($"{nameof(HistoryPage)}");
-    //}
 }
